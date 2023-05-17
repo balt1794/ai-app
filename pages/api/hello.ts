c2d47ab70619bd6406de14a07a502e19780d8fd5
balt1794 <bal22@njit.edu>
2023-05-17 10:05:24 -0400

    first commit

---
+++ pages/api/hello.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Configuration, OpenAIApi } from "openai";
+
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+){
+    const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: req.body.text,
+        temperature: 0.7,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      });
+      res.status(200).json({result: completion.data});
+}
+
+
---
+++ pages/index.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react"
+import "../app/globals.css"
+export default function Home() {
+
+  const [value, setValue] = React.useState<string>("");
+  const [completion, setCompletion] = React.useState<string>("");
+
+  const handleInput = React.useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(e.target.value);
+    }, []
+  );
+
+  const handleOnClick = async () =>{
+    setCompletion("Loading...");
+    const response = await fetch('/api/hello', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ text: value }),
+    });
+    const data = await response.json();
+    setValue('');
+    setCompletion(data.result.choices[0].text)
+  };
+  
+  return (
+  <div className="flex flex-col items-center justify-center min-h-screen bg-sky-100">
+    <div className="mb-4">
+      <h2 className="text-2xl font-bold">Enter a prompt</h2>
+    </div>
+    <input className="px-4 py-2 border border-gray-300 rounded-md w-64 mb-4" value={value} onChange={handleInput} />
+    <button className="bg-black p-3 text-white hover:bg-gray-600 rounded-full"onClick={handleOnClick}>Generate</button>
+    <div className="mt-4">
+      <h2 className="text-2xl font-bold">Output:{completion}</h2>
+    </div>
+  </div>
+  );
+}
