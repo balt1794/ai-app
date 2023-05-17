d8f8cff942a4d89cffb294d5c92e8f83ba7547a7
balt1794 <bal22@njit.edu>
2023-05-17 10:26:37 -0400

    troubleshoot

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,26 +1,32 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Configuration, OpenAIApi } from "openai";
 
-
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-){
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
     const completion = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: req.body.text,
-        temperature: 0.7,
-        max_tokens: 256,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-      });
-      res.status(200).json({result: completion.data});
+      model: "text-davinci-003",
+      prompt: req.body.text,
+      temperature: 0.7,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+
+    console.log("Completion response:", completion);
+
+    res.status(200).json({ result: completion.data });
+  } catch (error) {
+    console.error("Error occurred:", error);
+
+    res.status(500).json({ error: "An error occurred while processing the request" });
+  }
 }
-
-
