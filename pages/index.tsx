eee6d8a567f450c447271c10baccd47bd625a478
balt1794 <bal22@njit.edu>
2023-05-17 13:44:38 -0400

    fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   };
 
   const handleOnClick = async () => {
-    setCompletion("Loading...");
+    setCompletion("Loading..");
     try {
       const response = await fetch("/api/hello", {
         method: "POST",
